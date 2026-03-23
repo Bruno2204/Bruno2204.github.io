589d3e0a67b075b326c303592bcb7dfee24ae0cc
Bruno2204 <brunoegama@gmail.com>
2026-03-22 23:11:39 -0300

    Add Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://bruno2204.github.io',
+  // base: '/Bruno2204.github.io',
   vite: {
     plugins: [tailwindcss()]
   }
